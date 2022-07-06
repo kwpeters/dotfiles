9f44ebbf740297e289258fdca09ec899372eaf5c
Kevin Peters <kwpeters@ra.rockwell.com>
2022-07-06 09:10:29 -0400

    Installed Chocolately.

--- powershell_7/Microsoft.PowerShell_profile.ps1
+++ powershell_7/Microsoft.PowerShell_profile.ps1
@@ -139,3 +139,13 @@ Check-Path -Dir "C:\\Users\\kwpeters\\dev\\kwp\\tewl\\clitools\\dist-saved\\src"
 
 # Give an overview of local work that has not been committed and pushed.
 localWork C:\Users\kwpeters\dev\
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
