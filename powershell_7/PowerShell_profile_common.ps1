564c790619dc0056cd627ac4a6cfe046d82bf869
Kevin Peters <kwpeters@gmail.com>
2023-06-30 13:44:17 -0400

    Added PS function for capturing Windows Spotlight images.

--- powershell_7/PowerShell_profile_common.ps1
+++ powershell_7/PowerShell_profile_common.ps1
@@ -3,3 +3,9 @@ function serve {
     Write-Output "Running Node.js module 'serve'.  Use -l to specify port."
     npx serve
 }
+
+
+function Run-WindowsSpotlightImages {
+    $destDir = Join-Path ${Env:CLOUDHOME} "data\wallpaper\Windows_Spotlight"
+    windowsSpotlightImages.cmd $destDir
+}
