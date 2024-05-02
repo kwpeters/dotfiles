be0a029aea4b19a77076a0878db392ddbcd04076
Kevin Peters <kwpeters@ra.rockwell.com>
2024-05-02 16:04:25 -0400

    Added environment variable so that PlantUML can create large images.

--- powershell_7/PowerShell_profile_common.ps1
+++ powershell_7/PowerShell_profile_common.ps1
@@ -9,3 +9,7 @@ function Run-WindowsSpotlightImages {
     $destDir = Join-Path ${Env:CLOUDHOME} "data\wallpaper\Windows_Spotlight"
     windowsSpotlightImages.cmd $destDir
 }
+
+
+# Allow PlantUML to create *really* large images.
+$env:PLANTUML_LIMIT_SIZE = 8192
